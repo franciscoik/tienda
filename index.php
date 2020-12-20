deea0d78af1d8948dcd71a216c4f5c1c9793ef35
ikarospam <ikarospam@gmail.com>
2020-12-19 19:01:55 -0600

    primer comentario

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>inicio</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php echo "hola" ?>
+	<h1>hola</h1>
+</body>
+</html>
